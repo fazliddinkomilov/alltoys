33f83c66c413b29e159e63176d17baf4f8022bcf
fazliddinkomilov <fazliddinkomilov777@gmail.com>
2020-12-23 20:31:05 +0500

    creat app toys

--- alltoys/toys/models.py
+++ alltoys/toys/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 
+class ActiveObjectsManager(models.Model):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
 class User(models.Model):
+    is_active = models.BooleanField(default=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
+    active_object = ActiveObjectsManager()
 
 
 class Toy(models.Model):
