68a39dea4db3f1b009f91dccdcb710f38b775bb3
fazliddinkomilov <fazliddinkomilov777@gmail.com>
2020-12-23 17:57:26 +0500

    creat app toys

--- alltoys/toys/models.py
+++ alltoys/toys/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+
+class Toy(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Address(models.Model):
+    address = models.CharField(max_length=100)
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=10)
