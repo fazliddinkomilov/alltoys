4dafec66feb9d30b29ae1e5dca863682f110ae6c
fazliddinkomilov <fazliddinkomilov777@gmail.com>
2020-12-27 16:58:02 +0500

    creat models and migrations

--- alltoys/toys/models.py
+++ alltoys/toys/models.py
@@ -6,26 +6,32 @@ class ActiveobjectsManager(models.Manager):
         return super().get_queryset().filter(is_active=True)
 
 
+class Address(models.Model):
+    address = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    zip_code = models.CharField(max_length=100, null=True, blank=True)
+
+
 class User(models.Model):
     is_active = models.BooleanField(default=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
+    address = models.OneToOneField(Address, on_delete=models.PROTECT)
 
     objects = models.Manager()
 
-    active_objects = ActiveobjectsManager
+    active_objects = ActiveobjectsManager()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
 
 
 class Toy(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, related_name="toys", on_delete=models.CASCADE, null=True, blank=True)
-    description = models.TextField( null=True, blank=True)
-
-
-class Address(models.Model):
-    address = models.CharField(max_length=100)
-
-
-class Tag(models.Model):
-    tag = models.CharField(max_length=10)
+    description = models.TextField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag, related_name="toys")
