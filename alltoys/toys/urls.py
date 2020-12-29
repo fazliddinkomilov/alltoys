2ea4f2148373bd0aae088414b3e24848b5a7650a
fazliddinkomilov <fazliddinkomilov777@gmail.com>
2020-12-29 14:47:01 +0500

    Updatend

--- alltoys/toys/urls.py
+++ alltoys/toys/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = "toys"
 urlpatterns = [
-    path('', views.dashboard, name="dashboard"),
+  #  path('', views.dashboard, name="dashboard"),
     path('users/', views.show_users, name="show_users"),
 ]
--- alltoys/toys/views.py
+++ alltoys/toys/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-from alltoys.toys.models import User, Toy
+from toys.models import User, Toy
+
 
 
 def show_users(request):
     users = User.objects.all()
     toys = Toy.objects.all()
-    return render(request, "toys\show_users.html", context={"users": users, "toys": toys})
+    return render(request, 'toys/show_users.html', context={"users_list": users, "toys": toys})
