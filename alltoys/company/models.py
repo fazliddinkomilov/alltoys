ef946a53a3c24a3352a96e1b1f82fbfebd9f002e
fazliddinkomilov <fazliddinkomilov777@gmail.com>
2021-02-05 20:20:46 +0500

    all_toys

--- alltoys/company/models.py
+++ alltoys/company/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 
+class ActiveobjectsMaмук(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
 class Company(models.Model):
-    salary = models.IntegerField() + '$'
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
 
 
 class Employee(models.Model):
@@ -10,3 +17,6 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
     destination = models.CharField(max_length=50)
+    salary = models.DecimalField(max_digits=10,decimal_places=2)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="employees")
+    description = models.TextField()
